978c787e8844e4d5c483086843cc43a7b76b4b7f
NeutronStellar <93152199+NeutronStellar@users.noreply.github.com>
2023-01-08 16:43:19 -0700

    Update Class.h

--- Class.h
+++ Class.h
@@ -1,16 +1,17 @@
 #ifndef Class_h
 #define Class_h
 
+#include <stddef.h>
 typedef struct Class {
 	size_t size;
 	const struct Class *super;
 	unsigned (*hash)(const void *);
 	int (*cmp)(const void *, const void *);
-	void *(*dup)(const void *);
-	void *init;
+	void *(*cpy)(void *, const void *), *(*dup)(const void *), *init, *new, *(*ref)(void *), *(*unref)(void *);
 } Class;
 typedef struct Object {
 	const struct Class *class;
+	long refcount;
 } Object;
 extern const Class ObjectClass;
 int instanceof(const void *, const void *), Object_cmp(const void *, const void *);
